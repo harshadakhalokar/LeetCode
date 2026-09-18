ec6b6b01f75b8f67572035dbe976c40f5525155c
Harshada Khalokar <harshadakhalokar@gmail.com>
2026-09-18 09:28:33 +0530

    Time: 1 ms (100%), Space: 48.6 MB (86.04%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,48 +1,43 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int[] ans=new int[nums1.length+nums2.length];
+        int total_length=nums1.length+nums2.length;
 
-        int i=0;
-        int j=0;
-        int k=0;
-        while(i<nums1.length && j<nums2.length){
-            if(nums1[i]<=nums2[j]){
-                ans[k]=nums1[i];
-                k++;
-                i++;
+        if(nums1.length>nums2.length){
+            int[] temp=nums1;
+            nums1=nums2;
+            nums2=temp;
+        }
+
+        int left=0;
+        int right=nums1.length;
+        
+        while(left<=right){
+            int partition1=(left+(right-left)/2);
+            int partition2=(total_length+1)/2-partition1;
+
+            int left1=(partition1==0) ? Integer.MIN_VALUE : nums1[partition1-1];
+            int right1=(partition1==nums1.length) ? Integer.MAX_VALUE : nums1[partition1];
+
+            int left2=(partition2==0) ? Integer.MIN_VALUE: nums2[partition2-1];
+            int right2=(partition2==nums2.length) ? Integer.MAX_VALUE : nums2[partition2];
+
+            if(left1<=right2 && left2<=right1){
+                if(total_length%2 != 0){
+                    double median=Math.max(left1,left2);
+                    return median;
+                }
+                else{
+                    double median=(Math.max(left1,left2)+Math.min(right1,right2))/2.0;
+                    return median;
+                }
             }
-            else{
-                ans[k]=nums2[j];
-                k++;
-                j++;
+            else if(left1>right2){
+                right=partition1-1;
+            }
+            else if(left2>right1){
+                left=partition1+1;
             }
         }
-
-        while(i<nums1.length){
-            ans[k]=nums1[i];
-            k++;
-            i++;
-        }
-
-        while(j<nums2.length){
-            ans[k]=nums2[j];
-            k++;
-            j++;
-        }
-
-        int sum=0;
-        for(int x=0;x<ans.length;x++){
-            sum=sum+ans[x];
-        }
-
-        double avg=0;
-        if(ans.length%2!=0){
-            avg=ans[ans.length/2];
-        }
-        else{
-            avg=(ans[ans.length/2-1]+ans[ans.length/2])/2.0;
-        }
-
-        return avg;
+        return -1;
     }
 }
